c9c5d8007a10bb44b9b32726f209dbe474bb20a3
Sahar380 <118069703+Sahar380@users.noreply.github.com>
2022-11-13 08:52:14 -0800

    Add files via upload

---
+++ p5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+main() {
+  char English;
+  cout<<"please Enter an English letter;";
+  cin>> English;
+  
+  switch  (English){
+   case 'a':
+   	cout<<"VOWEL";
+  	break;
+   	case 'e':
+  	cout<<"VOWEL";
+  	break;
+  	case 'i':
+    cout<<"VOWEL";
+    break;
+    case 'o':
+    cout<<"VOWEL";
+    break;
+    case 'u':
+    cout <<"VOWEL";
+    break;
+}
+}
+
+
+
+
