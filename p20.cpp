b85c48114f8e7e8ad4b1e84e75bb95b08e282bd7
Sahar380 <118069703+Sahar380@users.noreply.github.com>
2022-11-27 11:10:58 -0800

    Add files via upload

---
+++ p20.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ 
+ main(){
+ 
+ int n,m,i,bmm;
+ cout << "please enter first integer number: ";
+ cin  >> n;
+ cout << "please enter second integer number :" ;
+ cin  >> m;
+ 
+  i=1;
+   while( i<=n ) {
+    
+    while( i<=m) { 
+	} 
+	i++;
+	if(n%i==0 && m%i==0 )
+	bmm =i;
+	
+  }
+  cout << "bmm between: " << m << " "<< "and " << m << " "<< "is";
+    }
